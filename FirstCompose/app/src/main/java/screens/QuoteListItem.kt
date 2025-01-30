aaf5734df60097d40bac78328b8e7eaa777ef183
sumit <sumokhan76342@gmail.com>
2025-01-30 23:11:51 +0530

    QuoteComposeApp - 1

---
+++ FirstCompose/app/src/main/java/screens/QuoteListItem.kt
@@ -0,0 +1,128 @@
+package screens
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FormatQuote
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.firstcompose.R
+
+@Composable
+private fun QuoteListItem() {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Row(modifier = Modifier.padding(16.dp)) {
+            Image(
+                imageVector = Icons.Filled.FormatQuote,
+                contentDescription = "Quote",
+                colorFilter = ColorFilter.tint(Color.White),
+                modifier = Modifier
+                    .background(Color.Black)
+                    .size(44.dp)
+                    .rotate(180F)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Column {
+                Text(
+                    text = "Time is the valuable things a man can spend.",
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp)
+                )
+                Box(
+                    modifier = Modifier
+                        .height(1.dp)
+                        .background(Color.Gray)
+                        .fillMaxSize(.4f)
+                )
+                Text(
+                    text = "Theophrastus",
+                    fontWeight = FontWeight.Thin,
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun QuoteDetail() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xFFffffff),
+                        Color(0xFFE3E3E3)
+                    )
+                )
+            )
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(4.dp),
+            colors = CardDefaults.cardColors(Color.White),
+            modifier = Modifier.padding(32.dp)
+                .background(Color.White)
+
+
+        ) {
+            Column (
+                modifier = Modifier.padding(16.dp,24.dp)
+
+            ){
+                Image(
+                    imageVector = Icons.Filled.FormatQuote,
+                    contentDescription = "Quote",
+                    modifier = Modifier
+                        .rotate(180f)
+                        .size(80.dp)
+                )
+                Text(
+                    text = "Time is the valuable things a man can spend.",
+                    fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "Theophrastus",
+                    fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+                    style = MaterialTheme.typography.labelLarge
+                )
+            }
+        }
+    }
+}
